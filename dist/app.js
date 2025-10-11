40ca8a0d247086297a59c1c951508cb736e77a2f
Uk <umairk83@live.com>
2025-10-12 01:05:23 +0500

    Added some more code

--- dist/app.js
+++ dist/app.js
@@ -2,4 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Hello World");
 console.log("Another hello world");
+class StaticClass {
+    static count = 0;
+    static incrementCount() {
+        return this.count++;
+    }
+}
+console.log(StaticClass.incrementCount());
 //# sourceMappingURL=app.js.map

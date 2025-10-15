43c0155c4a694d49f603f9e2168278768b2705c7
Uk <umairk83@live.com>
2025-10-15 15:16:08 +0500

    Added ts

---
+++ namespace.ts
@@ -0,0 +1,17 @@
+
+namespace MathOperations {
+    export function add(a: number, b: number) {
+
+        return a + b;
+    }
+}
+
+namespace StringOperations {
+    export function add(a: string, b: string) {
+        return a + b;
+    }
+}
+
+
+
+console.log(StringOperations.add("a", "b"));
